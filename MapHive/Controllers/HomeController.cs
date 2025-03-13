33ec4928fbbadf6fcbc2e4f1d457764947bcde57
Martin Urubek <urubek@allium.cz>
2025-03-13 20:49:56 +0100

    #4 Add login and register page cleanup

--- MapHive/Controllers/HomeController.cs
+++ MapHive/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-using MapHive.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace MapHive.Controllers
 {
@@ -21,12 +19,6 @@ namespace MapHive.Controllers
         public IActionResult Privacy()
         {
             return this.View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
         }
     }
 }
